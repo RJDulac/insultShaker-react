5f466ed321ec81ffc1c6d198e58dfd3abfe11ac0
Ryan Dulac <analyticrye@gmail.com>
2019-02-02 20:56:46 -0500

    button added and more changes - make button generate later

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,21 @@ import React from "react";
 import RandomWordList from "./RandomWordList";
 
 class App extends React.Component {
+  handleGenerate() {
+    console.log("button pressed");
+  }
   render() {
     return (
       <div className="App">
-        <RandomWordList />
+        <div style={{ textAlign: "center", marginBottom: "4rem" }}>
+          <RandomWordList />
+        </div>
+        <button
+          onClick={this.handleGenerate}
+          style={{ display: "block", margin: "auto" }}
+        >
+          Generate Insult
+        </button>
       </div>
     );
   }
