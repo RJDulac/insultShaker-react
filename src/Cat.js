b1eae9f5919a6c51be9328bf4a9a2f779acdf5a8
Ryan Dulac <analyticrye@gmail.com>
2019-02-02 20:03:52 -0500

    basics of insult shaker implemented

--- src/Cat.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Cat = props => {
-  return <div>Testing {props.turtle}</div>;
-};
-
-export default Cat;
---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import CombineWords from "./CombineWords";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <CombineWords />
+      </div>
+    );
+  }
+}
+
+export default App;
